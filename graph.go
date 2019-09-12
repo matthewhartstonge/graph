12528a6171f75fae1a477ef109f03c2e36b30333
Matthew Hartstonge <matt@mykro.co.nz>
2019-09-13 00:43:47 +1200

    :arrow_up: csp: migrates to using interfaces.

--- graph.go
+++ graph.go
@@ -5,6 +5,8 @@ import (
 )
 
 type Grapher interface {
+	GetFrontier()
+	GetPaths()
 	AddEdge()
 	AddNode()
 }
@@ -12,10 +14,11 @@ type Grapher interface {
 // NewGraph creates a new graph.
 func NewGraph(options ...GraphOption) *Graph {
 	g := &Graph{
-		V:       []*Vertex{},
-		E:       []*Edge{},
-		digraph: false,
-		isGoal:  nil,
+		V:        []Vertexer{},
+		E:        []Edger{},
+		digraph:  false,
+		Strategy: nil,
+		Goal:     nil,
 	}
 
 	for _, option := range options {
@@ -29,20 +32,31 @@ func NewGraph(options ...GraphOption) *Graph {
 type GraphOption func(g *Graph)
 
 // GoalFunc provides the algorithm to check if this is the goal vertex.
-type GoalFunc func(v *Vertex) bool
+type GoalFunc func(v Vertexer) bool
+
+// TODO: use Grapher interface instead.
+// TODO: document SearchStrategy.
+type SearchStrategy func(V []Vertexer, E []Edger) *Vertex
 
 // WithVertices provides a way to supply your own vertices when creating a new
 // graph.
-func WithVertices(vertices []*Vertex) GraphOption {
+func WithVertices(vertices []Vertexer) GraphOption {
 	return func(g *Graph) {
 		g.V = vertices
 	}
 }
 
 // WithEdges provides a way to supply your own edges when creating a new graph.
-func WithEdges(edges []*Edge) GraphOption {
+func WithEdges(edges []Edger) GraphOption {
 	return func(g *Graph) {
 		g.E = edges
+	}
+}
+
+// WithSearchStrategy provides a way to inject a graph search algorithm.
+func WithSearchStrategy(f SearchStrategy) GraphOption {
+	return func(g *Graph) {
+		g.Strategy = f
 	}
 }
 
@@ -50,23 +64,27 @@ func WithEdges(edges []*Edge) GraphOption {
 // the graph search.
 func WithGoalFunc(f GoalFunc) GraphOption {
 	return func(g *Graph) {
-		g.isGoal = f
+		g.Goal = f
 	}
 }
 
 // Graph provides the data structure for a Graph.
 // G = (V, E)
 type Graph struct {
-	// V contains a set of vertices, also called nodes.
-	V []*Vertex
-	// E contains a set of edges, also called links.
-	E []*Edge
 	// digraph provides a check to see if the graph is a directed or
 	// undirected.
 	digraph bool
-	// isGoal contains the algorithm to check if a given vertex satisfies the
+
+	// V contains a set of vertices, also called nodes.
+	V []Vertexer
+	// E contains a set of edges, also called links.
+	E []Edger
+
+	// Goal contains the algorithm to check if a given vertex satisfies the
 	// goal state.
-	isGoal GoalFunc
+	Goal GoalFunc
+	// Strategy provides the search strategy algorithm.
+	Strategy SearchStrategy
 }
 
 func (g *Graph) preprocess() {
@@ -97,17 +115,17 @@ func (g Graph) PrintInfo() {
 	}
 }
 
-func printDescendants(vertex *Vertex) string {
+func printDescendants(vertex Vertexer) string {
 	line := ""
-	for _, child := range vertex.children {
+	for _, child := range vertex.Children() {
 		line = fmt.Sprintf(" = parent of => %s", printDescendants(child))
 	}
 	return fmt.Sprintf("(%s)%s", vertex.Label, line)
 }
 
-func printHeritage(vertex *Vertex) string {
+func printHeritage(vertex Vertexer) string {
 	line := ""
-	for _, parent := range vertex.parents {
+	for _, parent := range vertex.Parents() {
 		line = fmt.Sprintf(" = child of => %s", printHeritage(parent))
 	}
 	return fmt.Sprintf("(%s)%s", vertex.Label, line)
--- vertex.go
+++ vertex.go
@@ -1,20 +1,24 @@
 package csp
 
-func NewVertex(label string) *Vertex {
+func NewVertex(label string) Vertexer {
 	return &Vertex{
-		Label: label,
+		label: label,
 		// Value: value,
 		visited:  false,
-		parents:  []*Vertex{},
-		children: []*Vertex{},
+		parents:  []Vertexer{},
+		children: []Vertexer{},
 	}
 }
 
 type Vertexer interface {
+	Label() string
+	SetLabel(label string)
+	Children() []Vertexer
+	AddChild(vertexer Vertexer) Vertexer
+	Parents() []Vertexer
+	AddParent(vertexer Vertexer) Vertexer
 	Visited() bool
-	SetVisited()
-	AddChild(v *Vertex) *Vertex
-	AddParent(v *Vertex) *Vertex
+	SetVisited(visited bool)
 }
 
 // Vertex provides the data structure for a node, or point, within a given
@@ -23,36 +27,31 @@ type Vertexer interface {
 // When solving a state-space problem, a vertex represents a state.
 type Vertex struct {
 	// Label provides the name of the vertex.
-	Label string
-	// Value provides the vertex's value.
-	Value int
+	label string
+	// children contains the vertices this vertex is a parent of.
+	children []Vertexer
+	// parents contains the vertices that this vertex links to.
+	parents []Vertexer
 	// visited specifies if the node has been visited.
 	visited bool
-	// parents contains the vertices that this vertex links to.
-	parents []*Vertex
-	// children contains the vertices this vertex is a parent of.
-	children []*Vertex
+
+	// Value provides the vertex's value.
+	Value int
 }
 
-func (v Vertex) Visited() bool {
-	return v.visited
+func (v Vertex) Label() string {
+	return v.label
 }
 
-func (v *Vertex) SetVisited() {
-	v.visited = true
+func (v *Vertex) SetLabel(label string) {
+	v.label = label
 }
 
-func (v *Vertex) AddParent(vertex *Vertex) *Vertex {
-	p, found := addToSet(v.parents, vertex)
-	if found {
-		return v
-	}
-
-	v.parents = p
-	return vertex.AddChild(v)
+func (v Vertex) Children() []Vertexer {
+	return v.children
 }
 
-func (v *Vertex) AddChild(vertex *Vertex) *Vertex {
+func (v *Vertex) AddChild(vertex Vertexer) Vertexer {
 	c, found := addToSet(v.children, vertex)
 	if found {
 		return v
@@ -62,10 +61,32 @@ func (v *Vertex) AddChild(vertex *Vertex) *Vertex {
 	return vertex.AddParent(v)
 }
 
+func (v Vertex) Parents() []Vertexer {
+	return v.children
+}
+
+func (v *Vertex) AddParent(vertex Vertexer) Vertexer {
+	p, found := addToSet(v.parents, vertex)
+	if found {
+		return v
+	}
+
+	v.parents = p
+	return vertex.AddChild(v)
+}
+
+func (v Vertex) Visited() bool {
+	return v.visited
+}
+
+func (v *Vertex) SetVisited(visited bool) {
+	v.visited = visited
+}
+
 // addToSet ensures no duplicates are added to the array based on key.
-func addToSet(src []*Vertex, vertex *Vertex) (dst []*Vertex, found bool) {
+func addToSet(src []Vertexer, vertex Vertexer) (dst []Vertexer, found bool) {
 	for _, v := range src {
-		if v.Label == vertex.Label {
+		if v.Label() == vertex.Label() {
 			// node already added to parents.
 			return src, true
 		}

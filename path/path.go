e56ad8bb260062908d6aa148f2b1264057a0909e
Matthew Hartstonge <matt@mykro.co.nz>
2019-09-15 22:46:13 +1200

    :new: path: initial implementation of a path.

---
+++ path/path.go
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2019. Matthew Hartstonge <matt@mykro.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package path
+
+import (
+	"github.com/matthewhartstonge/graph/edge"
+)
+
+func New() *Path {
+	return &Path{
+		current: -1,
+		edges:   []edge.Edger{},
+	}
+}
+
+type Pather interface {
+	Append(edge edge.Edger)
+	Cost() float64
+	Next() edge.Edger
+	Prev() edge.Edger
+	Reset()
+}
+
+type Path struct {
+	cost    float64
+	current int
+	edges   []edge.Edger
+}
+
+func (p *Path) Append(edge edge.Edger) {
+	p.cost += edge.Cost()
+	p.edges = append(p.edges, edge)
+}
+
+func (p Path) Cost() float64 {
+	return p.cost
+}
+
+func (p *Path) Next() edge.Edger {
+	p.current++
+	if p.current >= len(p.edges) {
+		return nil
+	}
+
+	return p.edges[p.current]
+}
+
+func (p *Path) Prev() edge.Edger {
+	p.current--
+	if p.current <= 0 {
+		return nil
+	}
+
+	return p.edges[p.current]
+}
+
+func (p *Path) Reset() {
+	p.current = -1
+}
